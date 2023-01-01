892b47eac5f8f60334323816fb2f378b4fa17398
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-01 16:31:43 +0300

    add artist name in detail

--- src/presentation/components/artists/artist-detail/artist-detail-view/ArtistDetailView.js
+++ src/presentation/components/artists/artist-detail/artist-detail-view/ArtistDetailView.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import TopAlbums from "../albums/TopAlbums";
 import TopTracks from "../tracks/TopTracks";
 import styles from "./ArtistDetailView.module.css";
 
 function ArtistDetailView() {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const name = params.get("name");
+
   return (
-    <div className={styles.container}>
-      <div className={styles["album-list"]}>
-        <TopAlbums />
+    <>
+      <h1 className={styles.title}>{name} 's Best Tracks & Albums</h1>
+      <div className={styles.container}>
+        <div className={styles["album-list"]}>
+          <TopAlbums />
+        </div>
+        <div className={styles["track-list"]}>
+          <TopTracks />
+        </div>
       </div>
-      <div className={styles["track-list"]}>
-        <TopTracks />
-      </div>
-    </div>
+    </>
   );
 }
 
