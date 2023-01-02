ae5cac6f750cd5391abb09a5157901f452c0919a
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-02 13:28:54 +0300

    feat: add infinite scroll to artist list view

--- src/presentation/components/artists/artists-list/ArtistsListView.js
+++ src/presentation/components/artists/artists-list/ArtistsListView.js
@@ -1,50 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Card from "../../ui/card/Card";
-import styles from "./ArtistsListView.module.css";
-import config from "../../../../config/config";
-import useHttp from "../../../hooks/use-http";
-import Error from "../../common/error/Error";
-import Loading from "../../common/loading/Loading";
+import InfiniteScroll from "../../../components/infinite-scroll/InfiniteScroll.js";
 
 function ArtistListView() {
-  const url = `${config.defaultUrl}`;
-  const { data, loading, error } = useHttp(url);
-  const title = "Artist";
-
-  if (error) {
-    return <Error message={error.message} />;
-  }
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  const artistList = data?.artists?.artist;
-
-  return (
-    <div className={styles["list-view"]}>
-      {artistList.map((artist) => {
-        return (
-          <div key={artist.mbid + artist.name}>
-            <Link
-              to={`/artist/${artist.mbid}?name=${artist.name}`}
-              className={styles["link"]}
-            >
-              <Card
-                key={artist.mbid}
-                title={title}
-                image={artist.image[4]["#text"]}
-                label={artist.name}
-                count={artist.playcount}
-                listeners={artist.listeners}
-              />
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <InfiniteScroll />;
 }
 
 export default ArtistListView;
---
+++ src/presentation/components/infinite-scroll/InfiniteScroll.js.js
@@ -0,0 +1,92 @@
+import React, { useState, useRef, useEffect } from "react";
+import Card from "../ui/card/Card";
+import { Link } from "react-router-dom";
+import styles from "./InfiniteScroll.module.css";
+import config from "../../../config/config";
+import Button from "../ui/button/Button";
+
+const InfiniteScroll = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const observer = useRef(null);
+  const targetElement = useRef(null);
+  const btnLabel = "Load More";
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          loadData();
+        }
+      }
+    });
+
+    if (targetElement.current) {
+      observer.current.observe(targetElement.current);
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, []);
+
+  const loadData = () => {
+    if (!hasMore) {
+      return;
+    }
+
+    fetch(`${config.defaultUrl}&page=${page}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData([...data, ...res.artists.artist]);
+
+        if (!res.artists.artist) {
+          setHasMore(false);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (hasMore) {
+      loadData();
+    }
+  }, [page]);
+
+  return (
+    <div className={styles["list-view"]}>
+      {data.map((artist) => {
+        return (
+          <div key={"-link-" + artist.mbid + "--" + artist.name}>
+            <Link
+              to={`/artist/${artist.mbid}?name=${artist.name}`}
+              className={styles.link}
+            >
+              <Card
+                key={artist.mbid + artist.name}
+                title={artist.name}
+                image={artist.image[4]["#text"]}
+                label={artist.name}
+                count={artist.playcount}
+                listeners={artist.listeners}
+              />
+            </Link>
+          </div>
+        );
+      })}
+      {hasMore && (
+        <div
+          className={styles["btn-wrapper"]}
+          ref={targetElement}
+          onClick={() => setPage(page + 1)}
+        >
+          <Button label={btnLabel} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
