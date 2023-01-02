ce66fd9a9abd8efe6cd5be463db6229c8e4344f6
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-02 16:50:30 +0300

    feat(card-component): add tests for props

--- src/__tests__/components/Card.test.js
+++ src/__tests__/components/Card.test.js
@@ -6,4 +6,36 @@ describe("card component", () => {
     const { container } = render(<Card />);
     expect(container).toMatchSnapshot();
   });
+
+  test("it should render image correctly", () => {
+    const imageUrl =
+      "https://cdn.britannica.com/48/201348-050-525ECEBB/Norah-Jones-2012.jpg";
+    const { container } = render(<Card image={imageUrl} />);
+    const imageElement = container.querySelector("img");
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute("src", imageUrl);
+  });
+
+  test("it should render title", () => {
+    const title = "Card Title";
+    const { getByText } = render(<Card title={title} />);
+    expect(getByText(title)).toBeInTheDocument();
+  });
+  test("it should render label", () => {
+    const label = "Card Label";
+    const { getByText } = render(<Card title={label} />);
+    expect(getByText(label)).toBeInTheDocument();
+  });
+
+  test("it should render count", () => {
+    const count = 12312;
+    const { getByText } = render(<Card title={count} />);
+    expect(getByText(count.toString())).toBeInTheDocument();
+  });
+
+  test("it should render listeners", () => {
+    const listeners = 324234;
+    const { getByText } = render(<Card title={listeners} />);
+    expect(getByText(listeners.toString())).toBeInTheDocument();
+  });
 });
